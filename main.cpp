2486dd3c7f1292e483835447a7ac49d02d47f13e
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2023-01-12 19:22:46 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,189 @@
+#include <string>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+// GLM Mathemtics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//texture load
+#include "SOIL2/SOIL2.h"
+
+// Properties
+const GLuint WIDTH = 800, HEIGHT = 600;
+int SCREEN_WIDTH, SCREEN_HEIGHT;
+
+// Function prototypes
+void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
+void MouseCallback( GLFWwindow *window, double xPos, double yPos );
+void DoMovement( );
+
+// Camera
+Camera camera( glm::vec3( 0.0f, 0.0f, 3.0f ) );
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+int main( )
+{
+    
+    glfwInit( );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+    
+    // window
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "3D", nullptr, nullptr );
+    
+    if ( nullptr == window )
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate( );
+        
+        return EXIT_FAILURE;
+    }
+    
+    glfwMakeContextCurrent( window );
+    
+    glfwGetFramebufferSize( window, &SCREEN_WIDTH, &SCREEN_HEIGHT );
+    
+    //callback
+    glfwSetKeyCallback( window, KeyCallback );
+    glfwSetCursorPosCallback( window, MouseCallback );
+    
+    // GLFW Options
+    glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+    
+    // retrieving function
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW
+    if ( GLEW_OK != glewInit( ) )
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    //viewport dimensions
+    glViewport( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
+    
+    // OpenGL options
+    glEnable( GL_DEPTH_TEST );
+    
+    // shaders
+    Shader shader( "res/shaders/model.vs", "res/shaders/model.frag" );
+    
+    // Load models
+    Model Model( "res/model/sun2.obj" );
+    
+    glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
+    
+
+    while( !glfwWindowShouldClose( window ) )
+    {
+        // frametime
+        GLfloat currentFrame = glfwGetTime( );
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
+        //events
+        glfwPollEvents( );
+        DoMovement( );
+        
+        // colorbuffer
+        glClearColor( 0.05f, 0.05f, 0.05f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        
+        shader.Use( );
+        
+        glm::mat4 view = camera.GetViewMatrix( );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "projection" ), 1, GL_FALSE, glm::value_ptr( projection ) );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "view" ), 1, GL_FALSE, glm::value_ptr( view ) );
+        
+        // Draw the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate( model, glm::vec3( 0.0f, -1.75f, 0.0f ) );
+        model = glm::scale( model, glm::vec3( 0.2f, 0.2f, 0.2f ) );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model ) );
+        Model.Draw( shader );
+        
+        
+        glfwSwapBuffers( window );
+    }
+    
+    glfwTerminate( );
+    return 0;
+}
+
+
+void DoMovement( )
+{
+    // movement
+    if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
+    {
+        camera.ProcessKeyboard( FORWARD, deltaTime );
+    }
+    
+    if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
+    {
+        camera.ProcessKeyboard( BACKWARD, deltaTime );
+    }
+    
+    if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
+    {
+        camera.ProcessKeyboard( LEFT, deltaTime );
+    }
+    
+    if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
+    {
+        camera.ProcessKeyboard( RIGHT, deltaTime );
+    }
+}
+
+// close window
+void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode )
+{
+    if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action )
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    
+    if ( key >= 0 && key < 1024 )
+    {
+        if ( action == GLFW_PRESS )
+        {
+            keys[key] = true;
+        }
+        else if ( action == GLFW_RELEASE )
+        {
+            keys[key] = false;
+        }
+    }
+}
+
+void MouseCallback( GLFWwindow *window, double xPos, double yPos )
+{
+    if ( firstMouse )
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+    // PC screen coordinates
+    GLfloat xOffset = xPos - lastX;
+    GLfloat yOffset = lastY - yPos;
+    
+    lastX = xPos;
+    lastY = yPos;
+    
+    camera.ProcessMouseMovement( xOffset, yOffset );
+}
